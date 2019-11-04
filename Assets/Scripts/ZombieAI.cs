3306b0a67910a4389ed16f316d419a024dee1c7b
Ashish1414 <ashubansal.ashishbansal@gmail.com>
2019-11-04 10:02:10 +0530

    enemy attack done

---
+++ Assets/Scripts/ZombieAI.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAI : MonoBehaviour
+{
+
+    public GameObject thePlayer;
+    public GameObject theEnemy;
+    public float enemySpeed = 0.01f;
+    public bool attackTrigger = false;
+    public bool isAttacking = false;
+    public AudioSource hurtSound1;
+    public AudioSource hurtSound2;
+    public AudioSource hurtSound3;
+    public int hurtgen;
+    public GameObject theFlash;
+
+    void Update()
+    {
+        transform.LookAt(thePlayer.transform);
+        if (attackTrigger == false)
+        {
+            enemySpeed = 0.01f;
+            theEnemy.GetComponent<Animation>().Play("Walk");
+            transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, enemySpeed);
+        }
+        if (attackTrigger == true && isAttacking == false)
+        {
+            enemySpeed = 0;
+            theEnemy.GetComponent<Animation>().Play("Attack");
+            StartCoroutine(InflictDamage());
+        }
+
+    }
+
+    void OnTriggerEnter()
+    {
+        attackTrigger = true;
+    }
+
+    void OnTriggerExit()
+    {
+        attackTrigger = false;
+    }
+
+
+    IEnumerator InflictDamage()
+    {
+        isAttacking = true;
+        hurtgen = Random.Range(1, 4);
+        if (hurtgen == 1)
+        {
+            hurtSound1.Play();
+        }
+        if (hurtgen == 2)
+        {
+            hurtSound2.Play();
+        }
+        if (hurtgen == 3)
+        {
+            hurtSound3.Play();
+        }
+        theFlash.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        theFlash.SetActive(false);
+        yield return new WaitForSeconds(1.1f);
+        GlobalHealth.currentHealth -= 2;
+        
+        yield return new WaitForSeconds(0.9f);
+        isAttacking = false;
+    }
+
+}
